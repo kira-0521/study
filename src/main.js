9ce20d1d81b24e1cf76f02bc370340c47eb7c2b5
Kira Tanaka <kiratanaka0521@gmail.com>
2021-02-24 19:33:55 +0900

    input component

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
+
+axios.defaults.baseURL =
+  "https://firestore.googleapis.com/v1/projects/studyapp-29cc7/databases/(default)/documents";
 
 createApp(App)
   .use(store)
---
+++ src/views/Input.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="container">
+    <div class="input-area">
+      <div class="input-area__time">
+        <label for="studyTime">勉強時間: </label>
+        <input id="studyTime" v-model="studyTime" type="tel" />時間
+      </div>
+      <div class="input-area__density">
+        <label for="studyDensity">勉強密度: </label>
+        <select id="studyDensity" v-model="studyDensity">
+          <option value="薄">薄</option>
+          <option value="普">普</option>
+          <option value="濃">濃</option>
+        </select>
+      </div>
+      <div class="input-area__content">
+        <label for="studyContent">勉強内容: </label>
+        <textarea id="studyContent" v-model="studyContent" placeholder="任意" />
+      </div>
+    </div>
+    <button @click="submitStudy">投稿する</button>
+    <button @click="getLocation">現在地を取得</button>
+    <ul v-for="post in posts" :key="post">
+      <li>{{ post.fields.studyTime.stringValue }}</li>
+      <li>{{ post.fields.studyDensity.stringValue }}</li>
+      <li>{{ post.fields.studyContent.stringValue }}</li>
+      <li>{{ post.fields.nowTime.stringValue }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      studyTime: "", // 時間
+      studyDensity: "", // 密度
+      studyContent: "", // 内容
+      latitude: 0,
+      longitude: 0,
+      posts: []
+    };
+  },
+  computed: {
+    getNow() {
+      const now = new Date();
+      const year = now.getFullYear();
+      const mon = now.getMonth() + 1;
+      const day = now.getDate();
+      const hour = now.getHours();
+      const min = now.getMinutes();
+      return `${year}年${mon}月${day}日${hour}時${min}分`;
+    }
+  },
+  created() {
+    axios.get("/posts").then(response => {
+      console.log(response);
+      this.posts = response.data.documents;
+      })
+      .catch(alert("エラーが出ています"));
+  },
+  methods: {
+    submitStudy() {
+      axios
+        .post("/posts", {
+          fields: {
+            studyTime: {
+              stringValue: this.studyTime
+            },
+            studyDensity: {
+              stringValue: this.studyDensity
+            },
+            studyContent: {
+              stringValue: this.studyContent
+            },
+            nowTime: {
+              stringValue: this.getNow
+            }
+            // latitude: {
+            //   stringValue: this.latitude
+            // },
+            // longitude: {
+            //   stringValue: this.longitude
+            // }
+          }
+        })
+        .then(() => {
+          this.latitude = 0;
+          this.longitude = 0;
+          this.studyTime = "";
+          this.studyDensity = "";
+          this.studyContent = "";
+        })
+        .catch(error => {
+          console.log(error.response);
+        });
+    }
+    // getLocation() {
+    //   if (!navigator.geolocation) {
+    //     alert(
+    //       "現在地情報を取得できませんでした。お使いのブラウザでは現在地情報を利用できない可能性があります。エリアを入力してください。"
+    //     );
+    //     return;
+    //   }
+
+    //   const options = {
+    //     // より精度の高い位置情報を取得する
+    //     enableHighAccuracy: false,
+    //     // エラーコールバックが呼び出されるまでの時間
+    //     timeout: 5000,
+    //     // 位置情報の有効期限
+    //     maximumAge: 0
+    //   };
+
+    //   // 成功CB・エラーCB
+    //   navigator.geolocation.getCurrentPosition(
+    //     this.success,
+    //     this.error,
+    //     options
+    //   );
+    // },
+    // success(position) {
+    //   console.log(position);
+    //   this.latitude = position.coords.latitude;
+    //   this.longitude = position.coords.longitude;
+    // },
+    // error(error) {
+    //   switch (error.code) {
+    //     case 1: //PERMISSION_DENIED
+    //       alert("位置情報の利用が許可されていません");
+    //       break;
+    //     case 2: //POSITION_UNAVAILABLE
+    //       alert("現在位置が取得できませんでした");
+    //       break;
+    //     case 3: //TIMEOUT
+    //       alert("タイムアウトになりました");
+    //       break;
+    //     default:
+    //       alert("現在位置が取得できませんでした");
+    //       break;
+    //   }
+    // }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.input-area {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
